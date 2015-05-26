08974f95bbf057744e32ff6c529a4c490c0aa4fe
Lennart Regebro <lregebro@redhat.com>
2015-05-26 21:55:05 +0200

    More tests

--- doctrine/code/__init__.py
+++ doctrine/code/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF-8 -*-
 import collections
 
+
 # Although Code is mutable, and a sequence, but does not implement all methods
 # of MutableSequence or Sequence. It only implements all methods from
 # Sized and Iterable.
@@ -8,12 +9,13 @@ import collections
 class Code(collections.Sized, collections.Iterable):
     """A lazy list interface for a file with code in it."""
 
-    def __init__(self, file, filetype, read_ahead=50):
+    def __init__(self, file, filetype, read_ahead=50, newline='\n'):
         self.file = file
         self.filetype = filetype
         self.lines = []
         self.widgets = [] # Cache for widgets
         self.read_ahead = read_ahead
+        self.newline = newline
 
     def __setitem__(self, index, value):
         self.lines[index] = value
@@ -24,11 +26,21 @@ class Code(collections.Sized, collections.Iterable):
         del self.widgets[index]
 
     def __getitem__(self, index):
+        if index < 0:
+            # We must now read in the whole file:
+            while True:
+                line = self.file.readline()
+                if not line:
+                    break
+                self.lines.append(line)
+                self.widgets.append(None)
+
         while index >= len(self):
             line = self.file.readline()
             if not line:
                 raise IndexError
-            self.append(line)
+            self.lines.append(line)
+            self.widgets.append(None)
 
         return self.lines[index]
 
@@ -40,11 +52,17 @@ class Code(collections.Sized, collections.Iterable):
 
     def insert(self, index, value):
         'S.insert(index, value) -- insert value before index'
+        # First we have to make sure that the line where we want to
+        # insert a line exists:
+        self[index]
+        # Now we can insert:
         self.lines.insert(index, value)
         self.widgets.insert(index, None)
 
     def append(self, value):
         'S.append(value) -- append value to the end of the sequence'
+        if not self[-1][-1] in ('\n', '\r'):
+            self[-1] = self[-1] + self.newline
         self.lines.append(value)
         self.widgets.append(None)
 
@@ -52,10 +70,12 @@ class Code(collections.Sized, collections.Iterable):
         'S.clear() -> None -- remove all items from S'
         self.lines = []
         self.widgets = []
-        self.file.seek(0)
+        self.file.seek(0, 2)
 
     def extend(self, values):
         'S.extend(iterable) -- extend sequence by appending elements from the iterable'
+        if not self[-1][-1] in ('\n', '\r'):
+            self[-1] = self[-1] + self.newline
         self.lines.extend(values)
         self.widgets.extend([None for x in values])
 
--- tests/test_editing.py
+++ tests/test_editing.py
@@ -4,7 +4,7 @@ import unittest
 from doctrine.code import Code
 
 
-class TestCode(unittest.TestCase):
+class TestCodeEditing(unittest.TestCase):
 
     def test_lazyness(self):
         f = io.StringIO(u'A text\nwith several\nlines')
@@ -16,9 +16,19 @@ class TestCode(unittest.TestCase):
     def test_getlines(self):
         f = io.StringIO(u'A text\nwith several\nlines')
         c = Code(f, 'text')
-        self.assertEqual(c[1], 'with several\n')
+        # Test that you can get lines out of order:
         self.assertEqual(c[2], 'lines')
+        self.assertEqual(c[0], 'A text\n')
+        self.assertEqual(c[1], 'with several\n')
+
+        # And you get an error if the line doesn't exist.
         self.assertRaises(IndexError, c.__getitem__, 3)
+
+        # You can iterate:
+        i = 0
+        for x in c:
+            i += 1
+        self.assertEqual(i, 3)
 
     def test_delete_code(self):
         f = io.StringIO(u'A text\nwith several\nlines')
@@ -34,3 +44,27 @@ class TestCode(unittest.TestCase):
         self.assertEqual(c[0], 'A texnes')
         self.assertRaises(IndexError, c.__getitem__, 1)
 
+    def test_add_lines(self):
+        f = io.StringIO(u'A text\nwith several\nlines')
+        c = Code(f, 'text')
+        c.insert(1, 'New line\n')
+        self.assertEqual(c[1], 'New line\n')
+        self.assertEqual(c[2], 'with several\n')
+
+        # If you add lines and the last line doesn't have
+        # a line feed, it must get one:
+        c.extend(['More\n', 'text'])
+        self.assertEqual(len(c), 6)
+        self.assertEqual(c[3], 'lines\n')
+
+        self.assertEqual(c[5], 'text')
+        c.append('A last line')
+        self.assertEqual(c[5], 'text\n')
+        self.assertEqual(len(c), 7)
+
+    def test_clear(self):
+        f = io.StringIO(u'A text\nwith several\nlines')
+        c = Code(f, 'text')
+        self.assertEqual(c[0], 'A text\n')
+        c.clear()
+        self.assertRaises(IndexError, c.__getitem__, 0)
